cfdbb2a75ae759219ad5b0a3bb6df4b2137856ce
steffanynaranjov <1792@holbertonschool.com>
2020-06-10 22:44:04 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-
-system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
 {
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, quote, 59);
+return (1);
+}
