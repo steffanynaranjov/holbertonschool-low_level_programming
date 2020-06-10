b74e1c7e9f3c38554318783484e5840633907c69
steffanynaranjov <1792@holbertonschool.com>
2020-06-10 22:40:20 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+
+system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+{
