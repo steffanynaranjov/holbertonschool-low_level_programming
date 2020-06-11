c39b39755b06c43cd8b6b6933376ff9554f8a454
steffanynaranjov <1792@holbertonschool.com>
2020-06-11 19:26:43 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alphabet = 'a';
+
+while (alphabet <= 'z')
+putchar(alphabet++);
+
+putchar('\n');
+return (0);
+}
