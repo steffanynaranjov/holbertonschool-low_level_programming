27b0917ce20e5935c709b9f00c5a57b4a4f6a95b
steffanynaranjov <1792@holbertonschool.com>
2020-06-15 23:20:47 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* print_sign - Determine the sign and the size of a integer
+* _putchar - Prints characters one by one
+* @n: The evaluated input
+*
+* Description: This determines the sign and if it is greater or less than cero
+* Return: 0 Value if it works
+*/
+int print_sign(int n)
+{
+if (n > 0)
+}
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+{
+else
+{
+_putchar(48);
+return (0);
+}
+}
