655e5c52f4e0ba51af78b8a43967c5e6b27ef1be
steffanynaranjov <1792@holbertonschool.com>
2020-06-16 05:09:15 +0000

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 /**
 * main - Print string with _putchar
+* _putchar - Print characters one by one
+* @void: Description of a empty parameter
 *
-* Return: Always 0 (Success)
+* Return: Always 0
 */
 int main(void)
 {
 char *c = "Holberton";
-while (*c) 
+
+while (*c)
 _putchar(*(c++));
+
 _putchar('\n');
 return (0);
 }
