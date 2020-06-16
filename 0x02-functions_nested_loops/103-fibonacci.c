1ad45bca9dc8ea31e4edf4fcde08b24022756d9f
steffanynaranjov <1792@holbertonschool.com>
2020-06-16 16:46:17 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - This print fibonacci numbers
+*
+* Description: computes & prints
+* Return: 0 Value
+*/
+int main(void)
+{
+int i = 1, j = 1, sum = 0;
+while (i < 4000000)
+{
+i = i + j;
+j = i - j;
+if (i % 2 == 0)
+sum += i;
+}
+printf("%d", sum);
+
+}
