b98a7ab1105480e516e19d7d44f2dad8536af21f
steffanynaranjov <1792@holbertonschool.com>
2020-06-16 05:14:40 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,14 @@
 */
 int print_sign(int n)
 {
-if (n > 0)
+if (n > 0 || n < 0)
+{
+int r = n > 0 ? 1 : -1;
+
+_putchar(r == 1 ? '+' : '-');
+return (r);
 }
-_putchar(43);
-return (1);
-}
-else if (n < 0)
-{
-_putchar(45);
-return (-1);
-{
-else
-{
-_putchar(48);
+
+_putchar('0');
 return (0);
-}
 }
