cb42efcb814b22164149db6f396091ec40a2a608
steffanynaranjov <1792@holbertonschool.com>
2020-06-16 02:01:31 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add - Adds to integer
+* @a: First integer input
+* @b: Second integer input
+* Description: This is going to add to integer values
+* Return: Sum value if it works
+*/
+int add(int a, int b)
+{
+int c = a + b;
+
+return (c);
+}
