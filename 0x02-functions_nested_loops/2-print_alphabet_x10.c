3c2abc4972dd4c076630cb93e27bd35e3b058a24
steffanynaranjov <1792@holbertonschool.com>
2020-06-16 01:07:15 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,22 @@
 #include "holberton.h"
 /**
-* print_alphabet_x10 - Entry point
+* print_alphabet_x10 - Print the alphabet 10 times in a row
+* _putchar - Prints characters one by one
+* @void: Description of a empty parameter
 *
-* Return: void
+* Description: This prints the alphabet 10 times
+* Return: 0 Value if it works
 */
 void print_alphabet_x10(void)
 {
 char x, y;
+
 for (y = 1; y <= 10; y++)
 {
 for (x = 'a'; x <= 'z'; x++)
+{
 _putchar(x);
+}
 _putchar('\n');
+}
 }
