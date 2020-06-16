94bca22d30d2177c5fb34360d42a5375d5a9a69e
steffanynaranjov <1792@holbertonschool.com>
2020-06-16 05:21:55 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 * times_table - Prints the 9 times table, starting with 0
 * @void: The evaluated input
 *
-* Return: 0 Value 
+* Return: 0 Value
 */
 void times_table(void)
 {
@@ -16,17 +16,17 @@ for (m = 0; m <= 9; m++)
 mult = n * m;
 if (m == 0 && mult < 10)
 {
-_putchar((mult) + '0');
+_putchar((mult) +'0');
 }
 else if (n == 0 && m != 0)
 {
 _putchar(' ');
-_putchar((mult) + '0');
+_putchar((mult) +'0');
 }
 else if (mult < 10)
 {
 _putchar(' ');
-_putchar((mult) + '0');
+_putchar((mult) +'0');
 }
 else
 {
