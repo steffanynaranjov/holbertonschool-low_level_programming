66988b97893f6ffb549f11814a8a84c1ac874609
steffanynaranjov <1792@holbertonschool.com>
2020-06-17 01:58:43 +0000

    fobonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,43 +8,18 @@
 */
 int main(void)
 {
-unsigned long int w = 0, x = 0, y = 1, z;
-unsigned long int x1, x2, y1, y2, z1, z2;
+unsigned long int suma = 1, a = 1, b;
+int i;
 
-while (w < 92)
+for (i = 2; i < 98; i++)
 {
-z = x + y;
-x = y;
-y = z;
-printf("%lu, ", z);
-w++;
+b = suma;
+suma = suma + a;
+a = b;
+printf("%lu", a);
+if (i != 99)
+printf(", ");
 }
-x1 = x / 10000000000;
-x2 = x % 10000000000;
-y1 = y / 10000000000;
-y2 = y % 10000000000;
-while (w < 98)
-{
-z1 = x1 + y1;
-z2 = x2 + y2;
-if (z2 > 10000000000)
-{
-z1 = z1 + 1;
-z2 = z2 % 10000000000;
-}
-if (w < 97)
-{
-printf("%lu%lu, ", z1, z2);
-x1 = y1;
-x2 = y2;
-y1 = z1;
-y2 = z2;
-}
-else
-{
-printf("%lu%lu\n", z1, z2);
-}
-w++;
-}
+printf("\n");
 return (0);
 }
