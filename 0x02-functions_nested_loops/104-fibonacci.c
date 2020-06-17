e8d386b35dacc859add04f164aab809a53afa0e5
steffanynaranjov <1792@holbertonschool.com>
2020-06-17 02:47:40 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,18 +8,43 @@
 */
 int main(void)
 {
-double x = 1, y = 1, z;
-int i;
+unsigned long int w = 0, x = 0, y = 1, z;
+unsigned long int x1, x2, y1, y2, z1, z2;
 
-for (i = 2; i < 98; i++)
+while (w < 92)
 {
-z = x;
-x = x + y;
+z = x + y;
+x = y;
 y = z;
-printf("%.0f", y);
-if (i != 97)
-printf(", ");
+printf("%lu, ", z);
+w++;
 }
-printf("\n");
+x1 = x / 10000000000;
+x2 = x % 10000000000;
+y1 = y / 10000000000;
+y2 = y % 10000000000;
+while (w < 98)
+{
+z1 = x1 + y1;
+z2 = x2 + y2;
+if (z2 > 10000000000)
+{
+z1 = z1 + 1;
+z2 = z2 % 10000000000;
+}
+if (w < 97)
+{
+printf("%lu%lu, ", z1, z2);
+x1 = y1;
+x2 = y2;
+y1 = z1;
+y2 = z2;
+}
+else
+{
+printf("%lu%lu\n", z1, z2);
+}
+w++;
+}
 return (0);
 }
