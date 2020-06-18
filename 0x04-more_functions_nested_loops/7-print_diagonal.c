bc43f94b5cc50962328595be8394e93390e227d4
steffanynaranjov <1792@holbertonschool.com>
2020-06-18 18:15:34 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,19 +3,21 @@
 * print_diagonal - Draws a diagonalt line in the terminal
 * _putchar - Prints characters one by one
 * @n: Parameter of times
-*
+* 
 * Description: Draws a diagonal line
 * Return: 0 Value
 */
 void print_line(int n)
 {
-char x = '\';
-int y;
+int y, z;
 
-for (y = 0; y <= n; y++)
+for (y = 0; y < y; y++)
 {
-if (y != 0)
-_putchar(x);
+for (z = 0; z < z; z++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
 }
+if (n < 1)
 _putchar('\n');
 }
