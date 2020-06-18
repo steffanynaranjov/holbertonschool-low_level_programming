f0d485a542493f2d383ef523c0a539d0207f2683
steffanynaranjov <1792@holbertonschool.com>
2020-06-18 17:08:58 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _islower - Entry point
+* @c: character to check
+*
+* Return: 1 if c is uppercase
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
