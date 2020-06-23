6be7234f267efb35a29d5d82cc552ead79ea7e64
steffanynaranjov <1792@holbertonschool.com>
2020-06-23 02:19:07 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_rev - Prints a string, in reverse
+* @s: The evaluated input
+*
+* Description: Prints a string, in reverse
+* Return: n value
+*/
+void print_rev(char *s)
+{
+int x = 0;
+for (; s[x] !='\0';)
+x++;
+--x;
+for (; x >= 0;)
+_putchar(s[x--]);
+
+_putchar('\n');
+}
