1d82f15a16b7e045db1ff69b45d09557e4075fe3
steffanynaranjov <1792@holbertonschool.com>
2020-06-25 17:46:11 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,13 @@ char *_strcat(char *dest, char *src)
 {
 int x = 0, y = 0;
 
-while (*(dest + x) != '\0')
+while (dest[x] != '\0')
 {
 x++;
 }
-while (*(src + y) != '\0')
+while (src[y] != '\0')
 {
-*(dest + x) = *(src + y);
+dest[x] = scr[y];
 x++;
 y++;
 }
