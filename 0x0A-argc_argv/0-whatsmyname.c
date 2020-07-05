1d005411d94a5daab19802118ca0335c0d637615
steffanynaranjov <1792@holbertonschool.com>
2020-07-05 17:29:19 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,10 +10,10 @@
 */
 int main(int argc, char *argv[])
 {
-int x;
-for (x = 0; x < argc; x++)
+int x = 0;
+for (; x < argc; x++)
 {
-printf("%s\n" argv[x]);
+printf("%s\n", argv[x]);
 }
 return (0);
 }
