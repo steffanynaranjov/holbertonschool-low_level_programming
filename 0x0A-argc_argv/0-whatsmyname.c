be09e31ce66300b6de177ac1142866c1b64c8bbb
steffanynaranjov <1792@holbertonschool.com>
2020-07-05 17:19:42 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Prints its name
+* @argc: Firts input int
+* @argv: Second input char
+*
+* Description: Program that prints its name followed by a new line
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int x;
+for(x = 0; x < argc; x++)
+{
+printf("%s\n" argv[x]);
+}
+return (0);
+}
