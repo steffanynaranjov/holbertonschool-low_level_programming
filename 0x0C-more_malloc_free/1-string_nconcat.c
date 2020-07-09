2f460b6edcf8e1d0de922e253a87fc3b46733cab
steffanynaranjov <1792@holbertonschool.com>
2020-07-09 19:48:47 +0000

    first6

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,10 +15,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *conc, *conct;
 unsigned int x, y;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+s1 = s1 ? s1 : "";
+s2 = s2 ? s2 : "";
 
 for (y = 0; s1[y]; y++)
 x++;
