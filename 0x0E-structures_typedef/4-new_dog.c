dce181a0b07624cd1d08efeaeb61f4571cd265bc
steffanynaranjov <1792@holbertonschool.com>
2020-07-13 21:12:31 +0000

    4.2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,8 +3,8 @@
 #include "dog.h"
 /**
 * new_dog - creat a new dog
-* @name: name char 
-* @age: age float 
+* @name: name char
+* @age: age float
 * @owner: name owner char
 *
 * Return: new dog struct
@@ -17,10 +17,10 @@ int x = 0, y = 0;
 if (new_dog == NULL)
 return (new_dog);
 
-for (x = 0; owner[x]; x++)
+for (; owner[x]; x++)
 ;
 
-for (y = 0; name[y]; y++)
+for (; name[y]; y++)
 ;
 
 (*new_dog).owner = malloc((x + 1) * sizeof(char));
@@ -29,7 +29,7 @@ for (y = 0; name[y]; y++)
 if ((*new_dog).owner == NULL || (*new_dog).name == NULL)
 {
 free((*new_dog).owner);
- free((*new_dog).name);
+free((*new_dog).name);
 free(new_dog);
 return (NULL);
 }
