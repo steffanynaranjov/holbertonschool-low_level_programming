ef8b279a1cf1ff470b154ec6a0e2d29e011c3d17
steffanynaranjov <1792@holbertonschool.com>
2020-07-15 17:30:55 +0000

    header 1.1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,16 +6,6 @@
 * Description: Function to a pointer
 */
 int _putchar(char c);
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
-int (*get_op_func(char *s))(int, int);
-
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 
 #endif /*_FUNCTION_P_H */
