33e7c6b56d307417feee4f2f83e474fdaed97682
steffanynaranjov <1792@holbertonschool.com>
2020-07-15 22:22:43 +0000

    3.3.1

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+* op_add - add two int
+* @a: number
+* @b: number
+*
+* Return: int
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - substract two int
+* @a: number
+* @b: number
+*
+* Return: int
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* op_mul - multiply two int
+* @a: number
+* @b: number
+*
+* Return: int
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div - divide two int
+* @a: number
+* @b: number
+*
+* Return: int
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+* op_mod - mod two int
+* @a: number
+* @b: number
+*
+* Return: int
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
