c63e5471a89dd54a663023f0a1381400cceb12c4
steffanynaranjov <1792@holbertonschool.com>
2020-07-15 15:01:38 +0000

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,6 +9,8 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 int (*get_op_func(char *s))(int, int);
+int _putchar(char c)
+
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
