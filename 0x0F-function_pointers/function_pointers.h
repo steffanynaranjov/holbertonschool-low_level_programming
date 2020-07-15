aead5e1efb8541c93e5b26aaf1ba232b28a3ff84
steffanynaranjov <1792@holbertonschool.com>
2020-07-15 17:21:25 +0000

    header act

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,13 @@
 #ifndef _FUNCTION_P_H
 #define _FUNCTION_P_H
+
+#include <stdio.h>
 /**
 * struct op - New type
 *
 * Description: Function to a pointer
 */
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
@@ -16,6 +19,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 
 #endif /*_FUNCTION_P_H */
