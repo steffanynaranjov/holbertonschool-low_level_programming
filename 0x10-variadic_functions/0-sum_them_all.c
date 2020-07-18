15e806dccb8c7e58199b5e930b62df973f3c818c
steffanynaranjov <1792@holbertonschool.com>
2020-07-18 17:45:39 +0000

    0.2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,12 +7,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list vlist;
-int sum = 0;
-unsigned int x = 0;
+unsigned int x = 0, sum = 0;
 
 va_start(vlist, n);
-if (n == 0)
-return (0);
 while (x < n)
 {
 sum += va_arg(vlist, int);
