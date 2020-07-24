bf9fcef5f0b0166aa6cf09aab7a32edf46bd4e01
steffanynaranjov <1792@holbertonschool.com>
2020-07-24 02:21:46 +0000

    100.5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
-#include "lists.h"
+#include <stdio.h>
 /**
  * Start_up - print before main
  * @void: empty par
  * Description: print a string before the main
  * Return: 0
  */
-void Start_up(void)
+void __attribute__ ((constructor)) Star_up()
+
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
