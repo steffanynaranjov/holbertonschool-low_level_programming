2e35aef5a1c843c7a4d8354ac60764194aeb916f
steffanynaranjov <1792@holbertonschool.com>
2020-07-24 00:28:06 +0000

    2.2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,32 +8,28 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = NULL;
-	char *d = NULL;
+	list_t *new;
+	int x = 0;
 
-	if (!(*head))
-	{
-		(*head) = malloc(sizeof(list_t));
-
-		if ((*head))
-		{
-			(*head)->str = strdup(str);
-			for ((*head)->len = 0; *str; (*head)->len++)
-				str++;
-		}
-		return ((*head));
-	}
-
-	new = malloc(sizeof(list_t));
-	d = strdup(str);
-	if (!new || !d)
+	if (!str)
 		return (NULL);
 
-	for (new->len = 0; *str; new->len++)
-		str++;
-	new->next = (*head);
-	(*head) = new;
-	new->str = d;
+	new = malloc(sizeof(list_t));
+	if (new)
+	{
+		new->str = strdup(str);
+		if (!new->str)
+		{
+			free(new);
+			return (NULL);
+	}
+	while (new->str[x])
+		x++;
+	new->len = x;
+	new->next = *head;
+	*head = new;
 
 	return (new);
+}
+	return (NULL);
 }
