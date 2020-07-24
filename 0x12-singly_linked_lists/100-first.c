76a4b904d4c24a75fd07f5b3b1490dd9d1a9122d
steffanynaranjov <1792@holbertonschool.com>
2020-07-24 01:50:31 +0000

    100 advance 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * Start_up - print before main
+ *
+ * Description: print a string before the main
+ * Return: number of elements
+ */
+void Start_up(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
