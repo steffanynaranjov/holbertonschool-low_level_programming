1daf043ed9a70c4e283b463f01868fdee520dff0
steffanynaranjov <1792@holbertonschool.com>
2020-07-24 01:57:16 +0000

    100.2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
  */
 void Start_up(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
