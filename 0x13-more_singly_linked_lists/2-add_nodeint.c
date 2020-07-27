85d6c9ed0fc0c0ade19a9be89d90fe0c1b04bd0b
steffanynaranjov <1792@holbertonschool.com>
2020-07-27 17:21:35 +0000

    2.1

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint - add new node
+ * @head: node head
+ * @n: integer
+ * Description: Add a new node
+ * return: New node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new)
+	{
+		new->next = *head;
+		new->n = n;
+		*head = new;
+
+	}
+	return (new);
+}
