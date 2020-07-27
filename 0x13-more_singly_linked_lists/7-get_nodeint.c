6b195e39c1a6c796f807311f258ee6cdd68ea3e3
steffanynaranjov <1792@holbertonschool.com>
2020-07-27 22:54:28 +0000

    7.1

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - add new node
+ * @head: node head
+ * @index: unsigned int
+ * Description: Add a new node
+ * return: New node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		if (i == index)
+			return (tmp);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
