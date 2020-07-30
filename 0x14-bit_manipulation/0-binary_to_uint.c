753daff6d836404ebd9de24891f04f1df1a4340d
steffanynaranjov <1792@holbertonschool.com>
2020-07-30 16:03:00 +0000

    0.2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * binary_to_unit - convert binary
+ * binary_to_uint - convert binary
  * @b: pointing to a string
  * Description: convert binary to unsigned
  * Return: The convert number
@@ -8,6 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
+
 	while (b && *b)
 	{
 		if (*b == '0' || *b == '1')
