77e73cf90511b3ad12cf9c502b0d73e8b4b5a41d
steffanynaranjov <1792@holbertonschool.com>
2020-07-31 02:11:49 +0000

    header 1

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,8 +2,10 @@
 #define _HOLBERTON_H
 
 #include <stdio.h>
+#include <math.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void _print_binary_(unsigned long int n);
 
 #endif
