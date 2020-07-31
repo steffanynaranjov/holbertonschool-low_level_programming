fa7e449fad914a0420a3222e99ce7a977ce106dd
steffanynaranjov <1792@holbertonschool.com>
2020-07-31 03:18:36 +0000

    4.1

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * clear_bit - sets the value of a int
+ * @n: integer
+ * @index: index starting 0
+ * Description: prints the binary representation of a number.
+ * Return: -1 or 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x;
+	int s = 1;
+
+	x = (sizeof(n) * 8) -  1;
+	if (index > x)
+		return (-1);
+	*n &= ~(s << index);
+	return (1);
+}
