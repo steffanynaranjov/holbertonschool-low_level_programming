fe5435d2ef5200a78bc1d1aba63e6fef23e17ecc
steffanynaranjov <1792@holbertonschool.com>
2020-07-31 02:12:14 +0000

    1.1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _print_binary_ - convert an integer to binary but not zero
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void _print_binary_(unsigned long int n)
+{
+	if (n)
+	{
+		_print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
+/**
+ * print_binary - prints the binary
+ * @n: integer
+ * Description: prints the binary representation of a number.
+ * Return: 1
+ */
+void print_binary(unsigned long int n)
+{
+	if (n)
+		_print_binary(n);
+	else
+		_putchar('0');
+}
