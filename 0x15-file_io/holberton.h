ce143870434c753e3377f08715e84a6ee88730e4
steffanynaranjov <1792@holbertonschool.com>
2020-08-04 17:40:25 +0000

    header 0

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
