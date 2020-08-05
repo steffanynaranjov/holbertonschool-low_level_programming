712cbe6abf618dd27dad969a8f4ed5a93255a71f
steffanynaranjov <1792@holbertonschool.com>
2020-08-05 02:17:25 +0000

    header 3.5

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,5 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
-
 #define SIZE 1024
 
 #include <sys/types.h>
@@ -13,7 +12,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
-int main(int ac, char **av);
 
 #endif
