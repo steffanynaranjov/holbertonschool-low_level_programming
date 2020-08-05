60289563b0de32bd11f008f70e90de2ff710b488
steffanynaranjov <1792@holbertonschool.com>
2020-08-05 02:13:08 +0000

    3.5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
 /**
- * main - Prototype
- * @ac: input int argument
- * @av: char argument of string
+ * main - Print the name
+ * @argc: input int argument
+ * @argv: char argument of string
  * Description: Copy a file
- * Return: -1 or 1
+ * Return: -1 or 0
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	int fd, fdw;
 	char buffer[SIZE], *file_from, *file_to;
 	ssize_t rd = 1024, wr;
 
-	if (ac != 3)
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	if (!av[1])
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(97);
-	file_from = av[1];
-	file_to = av[2];
+	if (!argv[1])
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(97);
+	file_from = argv[1];
+	file_to = argv[2];
 	fd = open(file_from, O_RDONLY);
 	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	fdw = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdw == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
