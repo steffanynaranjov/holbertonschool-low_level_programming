241664d9cbcc377cd6748a73ad9dc33b14603e33
steffanynaranjov <1792@holbertonschool.con>
2020-09-07 23:12:20 +0000

    third try ag

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/steffanynaranjov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/winner.so
-export LD_PRELOAD=/tmp/winner.so
+wget -P /tmp https://github.com/steffanynaranjov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
