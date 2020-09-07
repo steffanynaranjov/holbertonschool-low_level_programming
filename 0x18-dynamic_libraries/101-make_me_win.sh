14c71b9e37a060d02a7f751d9378d4b0431c3fa3
steffanynaranjov <1792@holbertonschool.con>
2020-09-07 23:52:09 +0000

    update again again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/steffanynaranjov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=/tmp/libwin.so
+wget -P /tmp https://github.com/steffanynaranjov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD=/tmp/liball.so
