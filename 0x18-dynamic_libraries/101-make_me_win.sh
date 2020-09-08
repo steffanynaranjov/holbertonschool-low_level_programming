63ac83635e526c86e604d1212b56f6eb8de1db2e
steffanynaranjov <1792@holbertonschool.con>
2020-09-08 01:15:37 +0000

    hdgjfff

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/steffanynaranjov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/liball.so
-export LD_PRELOAD=/tmp/liball.so
+wget -P /tmp https://github.com/steffanynaranjov/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/hacking.so
+export LD_PRELOAD=/tmp/hacking.so
